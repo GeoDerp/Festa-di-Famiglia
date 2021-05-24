f22c1961577845db607397d5a5884888401932f0
john whately <18461782+GeoDerp@users.noreply.github.com>
2021-05-25 00:42:15 +1000

    added support for catagory pagination
    Thanks to Aankhen for the .js help for this section of the code
    Co-authored-by: Aankhen Aankhen@users.noreply.github.com

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('css');
+
+  eleventyConfig.addCollection("recipeTags", async (collections) => {
+    const getRecipes = require("./_data/recipesContentful.js");
+    const recipes = await getRecipes();
+    return Array.from(new Set(recipes.flatMap((r) => r.categories)));
+  });
 }
+  
