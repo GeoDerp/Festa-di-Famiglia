1e45b091b07b719d4dfa09c2620d215a4a32cab3
GeoDerp <18461782+GeoDerp@users.noreply.github.com>
2021-05-25 15:23:39 +1000

    categories added alphabetical sort

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("recipeTags", async (collections) => {
     const getRecipes = require("./_data/recipesContentful.js");
     const recipes = await getRecipes();
-    return Array.from(new Set(recipes.flatMap((r) => r.categories)));
+    return Array.from(new Set(recipes.flatMap((r) => r.categories))).sort();
   });
 }
   
